efa1238c4ff95c7f8b3777c432f4fc3040cbb5a9
Raul Malavia <raulmalavia@hotmail.com>
2013-07-10 22:51:21 +0000

    web.js changed with index read

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
+buffer = new Buffer(27);
+buffer=fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
